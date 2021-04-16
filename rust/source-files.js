f745f180a505672fb730be7bd16f590018899ebb
David Drysdale <drysdale@google.com>
2021-04-16 10:42:43 +0100

    Update Rust docs to main branch at bc90d09545d4
    Auto-generated from commit bc90d09545d4 ("Mark iana enums as non-exhaustive").

---
+++ rust/source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["coset"] = {"name":"","dirs":[{"name":"common","files":["mod.rs"]},{"name":"context","files":["mod.rs"]},{"name":"encrypt","files":["mod.rs"]},{"name":"header","files":["mod.rs"]},{"name":"iana","files":["mod.rs"]},{"name":"key","files":["mod.rs"]},{"name":"mac","files":["mod.rs"]},{"name":"sign","files":["mod.rs"]},{"name":"util","files":["mod.rs"]}],"files":["lib.rs"]};
+createSourceSidebar();
